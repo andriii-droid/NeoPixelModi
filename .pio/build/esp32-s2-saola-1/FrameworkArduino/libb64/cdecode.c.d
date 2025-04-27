d25cc2a6e2d09cefaf97f04dd62e7a8d9226c00d
andriii-droid <andri.andermatt@icloud.com>
2025-04-27 20:50:40 +0200

    Create Files for Subclasses

---
+++ .pio/build/esp32-s2-saola-1/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32-s2-saola-1/FrameworkArduino/libb64/cdecode.c.o: \
+ /Users/andri/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ /Users/andri/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
---
+++ .pio/build/esp32-s2-saola-1/lib388/Adafruit NeoPixel/esp8266.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32-s2-saola-1/lib388/Adafruit\ NeoPixel/esp8266.c.o: \
+ .pio/libdeps/esp32-s2-saola-1/Adafruit\ NeoPixel/esp8266.c
