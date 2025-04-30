7667899513ab505f3d550e732f337008d3a44725
andriii-droid <andri.andermatt@icloud.com>
2025-04-30 23:11:21 +0200

    Add Files for derived Class MultiFade

---
+++ include/MultiFade.h
@@ -0,0 +1,14 @@
+//Datei: MultiFrame.h
+//Headerdatei für die SubKlasse MultiFade
+//Autor: Andri Andermatt, 27.04.2025
+#include <Arduino.h>
+#include <FrameFade.h>
+
+
+#ifndef MULTIFADE_H_
+#define MULTIFADE_H_
+    class MultiFade : public FrameFade
+    {
+
+    };
+#endif //MULTIFADE_H_
---
+++ src/MultiFade.cpp
@@ -0,0 +1,6 @@
+//Datei: MultiFade.cpp
+//Implementation für die SubKlasse MultiFade
+//Autor: Andri Andermatt, 27.04.202
+#include <Arduino.h>
+#include <MultiFade.h>
+
