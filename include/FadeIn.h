bffa645bf5cc4814e78e13acd127f119dfc86b92
andriii-droid <andri.andermatt@icloud.com>
2025-05-02 16:21:04 +0200

    Implement Headerfile

--- include/FadeIn.h
+++ include/FadeIn.h
@@ -6,5 +6,31 @@
 
 #ifndef FADEIN_H_
 #define FADEIN_H_
+    class FadeIn : public NeoPixelModi
+    {
+    public:
+    FadeIn(int initNumLed);
+    //Ctor
+
+    virtual ~FadeIn() {}
+
+    void run() override;
+    //Runs the FrameFade algorythm to generate PixelColors
+
+    void setColor(int newRed, int newGreen, int newBlue) override;
+    //Sets the Color
+
+    void correctValues(int& wRed, int& wGreen, int& wBlue);
+    //Sets the lowest Value to 0, the highest to 255
+
+    private:
+        enum fadeState {determine, redPlus, greenPlus, bluePlus, redMinus, greenMinus, blueMinus};
+        fadeState state = determine;
+        int count = 0;  
+
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+    };
 
 #endif //FADEIN_H_
