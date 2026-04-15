d26ea56b5ddc7eddad05ce20f7fa599ce7b55204
andriii-droid <andri.andermatt@icloud.com>
2026-04-15 11:04:29 +0200

    typo

--- src/pattern.cpp
+++ src/pattern.cpp
@@ -1,14 +1,14 @@
 #include <pattern.h>
 
-Pattern::Pattern(int numLed, std::vector<int> cont)
+Pattern::Pattern(int numLed, std::vector<int> frame)
 {
     patterns.push_back(new OneFrame{numLed});
     patterns.push_back(new FrameFade{numLed});
     patterns.push_back(new PixelRun{numLed});
-    patterns.push_back(new MultiFade{numLed, cont});
-    patterns.push_back(new MultiFrame{numLed, cont});
+    patterns.push_back(new MultiFade{numLed, frame});
+    patterns.push_back(new MultiFrame{numLed, frame});
     patterns.push_back(new FadeIn{numLed});
-    patterns.push_back(new MultiFadeIn{numLed, cont});
+    patterns.push_back(new MultiFadeIn{numLed, frame});
 
     numPattern = patterns.size();
 
